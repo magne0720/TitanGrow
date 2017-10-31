7d7e75c4a70116c74558fd2d28720613a4983047
矢田　英之 <nkc.hideyuki.yata@gmail.com>
2017-10-31 13:52:51 +0900

    プレイヤーの挙動追加

--- Assets/Resouces/Scripts/Player.cs
+++ Assets/Resouces/Scripts/Player.cs
@@ -5,38 +5,52 @@ using UnityEngine;
 public class Player : BaseCharacter {
     public int hp;                  //自身の体力
     public GameObject catchObject;  //つかんだもの
-    float z;
-   
+    private float z;                //自身のZ軸
+    public int FoodPoint;           //食ったものポイント 
+    private float timeElapsed;      //時間
 
 
 
     // Use this for initialization
     void Start () {
-        z = transform.position.z;
+        z = transform.position.z; //自身のZ軸を取得
 
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //投げる
         if (Input.GetKeyDown(KeyCode.U))
         {
             CastAway(catchObject);
         }
 
+        //離す
         if (Input.GetKeyDown(KeyCode.Y))
         {
             Release(catchObject);
         }
-	}
+
+        //食べる
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Eat(catchObject);
+        }
+
+        //大きくなる
+        Grow(FoodPoint); 
+    }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (catchObject == null)
         {
-            catchObject = collision.transform.gameObject;
-            Catch(catchObject);
+            if (collision.gameObject.tag == "Food")
+            {
+                catchObject = collision.transform.gameObject;
+                Catch(catchObject);
+            }
         }
-
     }
 
     //つかむ
@@ -50,8 +64,7 @@ public class Player : BaseCharacter {
     //投げる
     void CastAway(GameObject g)
     {
-        catchObject.GetComponent<Rigidbody>().AddForce(transform.forward * 1000.0f);
-        //catchObject.GetComponent<Rigidbody>().AddForce(0,1000.0f, 1000.0f);
+        catchObject.GetComponent<Rigidbody>().AddForce(transform.forward * 750.0f);
         g.transform.parent = null;
         catchObject = null;
     }
@@ -67,27 +80,43 @@ public class Player : BaseCharacter {
     void Eat(GameObject g)
     {
         //ポイントを得る処理
-        EatPoint(catchObject);
+        EatPoint(catchObject, FoodPoint);
 
         //食べたものを消す処理
         DestroyObject(g);
     }
 
     //食べた時のポイントを追加する
-    void EatPoint(GameObject g)
+    void EatPoint(GameObject g, int point)
     {
+        if(catchObject.name == "Capsule")
+        {
 
+            FoodPoint -= 4;
+        }
+
+        if (catchObject.name == "Cube")
+        {
+            FoodPoint -= 2;
+        }
+
+
+        if (FoodPoint == 6)
+        {
+            transform.localScale += new Vector3(-2.0f, -2.0f, -2.0f);
+        }
     }
 
     //自身の成長
-    void Grow()
+    void Grow(int point)
     {
-
+        //大きくなる
+        transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
     }
 
     //ダメージを受ける
     void Damege(int point)
     {
-
+        hp -= 1;
     }
 }
