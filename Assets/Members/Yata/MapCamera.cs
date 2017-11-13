95d30f1eaa58fc77980f7af86182ce36dca60d12
矢田　英之 <nkc.hideyuki.yata@gmail.com>
2017-11-13 19:14:07 +0900

    ミニマップ追加

---
+++ Assets/Members/Yata/MapCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCamera : MonoBehaviour {
+    public Transform target;
+
+    private const float _height = 100f;
+
+    void Start()
+    {
+        StartCoroutine(UpdatePosition());
+    }
+
+    IEnumerator UpdatePosition()
+    {
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+            transform.position = new Vector3(target.position.x, _height, target.position.z);
+            transform.eulerAngles = new Vector3(90f, 0f, -target.eulerAngles.y);
+        }
+    }
+}
