230abebd04b861161bd041d012d5f7664bd4a19f
ジョン卍ロー <nkc.kosuke.sato@gmail.com>
2017-11-27 15:09:27 +0900

    整列💩ミ卍

--- Assets/Resources/Scripts/Castle.cs
+++ Assets/Resources/Scripts/Castle.cs
@@ -8,8 +8,8 @@ public class Castle : MonoBehaviour {
     public int CastleHp = 1000;
 
     //生成する兵士
-    public GameObject soldier1;
-    public GameObject soldier2;
+    public string soldier1;
+    public string soldier2;
 
     //一度に生成する量
     private int soldier1Spawn = 5;
@@ -33,6 +33,9 @@ public class Castle : MonoBehaviour {
         SpawnTime = 0.0f;
         GoSortie = false;
 
+
+        soldier1 = "Prefabs/Enemy_CAL";
+        soldier2 = "Prefabs/Enemy_CAL";
     }
    
 	
@@ -60,26 +63,36 @@ public class Castle : MonoBehaviour {
     //兵士わくわくさん
     void Spawn()
     {
-        for (int unit = 0; unit < 3; unit++)
-        {
+
+        
+         for(int count=0;count<3;count++){
             for (int sol1 = 0; sol1 < soldier1Spawn; sol1++)
             {
-                Vector3 pos = new Vector3(2 * sol1 - 4, 0, 2 * unit);
-                GameObject.Instantiate(Enemy.Create(soldier1), pos, Quaternion.identity);
+                Vector3 pos = new Vector3(2 * sol1 - 4, 0, 2*count);
+                GameObject g=(Enemy.Create(soldier1));
+                g.transform.position = pos;
+                
+                
 
             }
-        }
+            
+
+
+            }
+        
 
 
         if (CastleHp <= 80)
         {
-
-            for (int sol2 = 0; sol2 < soldier2Spawn; sol2++)
-            {
-                Vector3 pos2 = new Vector3(3 * sol2 - 3, 5, 0);
-                GameObject.Instantiate(soldier2, pos2, Quaternion.identity);
+            for (int count2 =0; count2 < 2; count2++) {
+                for (int sol2 = 0; sol2 < soldier2Spawn; sol2++)
+                {
+                    Vector3 pos2 = new Vector3(3 * sol2 - 3, 5, 2*count2);
+                    GameObject g = (Enemy.Create(soldier2));
+                    g.transform.position = pos2;
+                }
             }
-
+            
         }
 
         SpawnTime = 0;
