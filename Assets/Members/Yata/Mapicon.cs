bbf7bcc9f2d3e47e638cfb7f345e9ab95a7c1c87
矢田　英之 <nkc.hideyuki.yata@gmail.com>
2017-12-01 15:16:00 +0900

    ミニマップ

---
+++ Assets/Members/Yata/Mapicon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mapicon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+        pos.y = 490;
+        transform.position = pos;
+
+	}
+}
