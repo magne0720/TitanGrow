add4eaf6937e3a20047cca9b6b883efc13b6bcbb
石原　寧央 <nkc.neo.ishihara@gmail.com>
2017-12-01 15:02:18 +0900

    human動き

--- Assets/Resources/Scripts/Human.cs
+++ Assets/Resources/Scripts/Human.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Human : MonoBehaviour {
+public class Human : BaseCharacter
+{
+    private float speed = 4.0f;
+    private Vector3 targetPosition;
+    private float moveTime;
+    private void Start()
+    {
+        targetPosition = GetRandomPosition();
+        moveTime = Random.Range(1,5);
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        moveTime -= Time.deltaTime;
+        //moveTimeが0以上なら行動する
+        if (moveTime > 0)
+        {
+            //正面に進む
+            this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPosition - this.transform.position);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime / 2);
+        }
+        //moveTimeが0以下なら次のポイントを設定する
+        if (moveTime < 0)
+        {
+            
+            moveTime = MoveTime();
+            targetPosition = GetRandomPosition();
+        }
+    }
+
+    public Vector3 GetRandomPosition()
+    {
+        float levelSize = 8.0f;
+        return new Vector3(Random.Range(-levelSize+1, levelSize), 0, Random.Range(-levelSize, levelSize));
+    }
+
+    public float MoveTime()
+    {
+        return Random.Range(1, 5);
+    }
 }
